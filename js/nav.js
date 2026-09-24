fc5e383db55e8bf900055056f40e91fe394dfa83
Loveleen Sarao <theclaritychamber@gmail.com>
2026-09-24 18:42:25 +1000

    Use Instagram, YouTube and TikTok icons in the footer

--- js/nav.js
+++ js/nav.js
@@ -72,13 +72,18 @@
   }
 
   var footer = document.querySelector("footer");
-  if (footer && !footer.querySelector(".footer-social")) {
+  if (footer) {
+    var existing = footer.querySelector(".footer-social");
+    if (existing) existing.remove();
+    var ig = '<svg viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm10 2H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-5 3.5A4.5 4.5 0 1 1 7.5 12 4.5 4.5 0 0 1 12 7.5zm0 2A2.5 2.5 0 1 0 14.5 12 2.5 2.5 0 0 0 12 9.5zM17.5 6a1 1 0 1 1-1 1 1 1 0 0 1 1-1z"/></svg>';
+    var yt = '<svg viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M23 12.2s0-3.2-.4-4.6a3 3 0 0 0-2.1-2.1C18.9 5.1 12 5.1 12 5.1s-6.9 0-8.5.4A3 3 0 0 0 1.4 7.6C1 9 1 12.2 1 12.2s0 3.2.4 4.6a3 3 0 0 0 2.1 2.1c1.6.4 8.5.4 8.5.4s6.9 0 8.5-.4a3 3 0 0 0 2.1-2.1c.4-1.4.4-4.6.4-4.6zM9.8 15.5V8.9l6.4 3.3z"/></svg>';
+    var tt = '<svg viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M14.2 3h2.6c.3 2.3 1.7 4.1 4 4.7v2.6c-1.4 0-2.7-.4-3.9-1.1v6.7A6.9 6.9 0 1 1 10 9.1v2.8a4.2 4.2 0 1 0 3 4v-12z"/></svg>';
     var social = document.createElement("p");
     social.className = "footer-social";
     social.innerHTML =
-      '<a href="https://www.instagram.com/theclaritychamber" rel="noopener noreferrer" target="_blank">Instagram</a>' +
-      '<a href="https://www.youtube.com/@theclaritychamber.official" rel="noopener noreferrer" target="_blank">YouTube</a>' +
-      '<a href="https://www.tiktok.com/@theclaritychamber" rel="noopener noreferrer" target="_blank">TikTok</a>';
+      '<a class="social-icon" href="https://www.instagram.com/theclaritychamber" rel="noopener noreferrer" target="_blank" aria-label="Instagram">' + ig + '</a>' +
+      '<a class="social-icon" href="https://www.youtube.com/@theclaritychamber.official" rel="noopener noreferrer" target="_blank" aria-label="YouTube">' + yt + '</a>' +
+      '<a class="social-icon" href="https://www.tiktok.com/@theclaritychamber" rel="noopener noreferrer" target="_blank" aria-label="TikTok">' + tt + '</a>';
     var contact = footer.querySelector(".footer-contact");
     if (contact && contact.nextSibling) footer.insertBefore(social, contact.nextSibling);
     else footer.appendChild(social);
